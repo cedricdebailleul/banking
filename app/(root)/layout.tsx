1372f9e2cf4b5aa70d3593c774cc3ce0938ae3a3
cedric debailleul <cedric.debailleul62@gmail.com>
2024-08-03 17:48:44 +0200

    📦 NEW:  rightsidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,7 +7,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { firstName: "Adrian", lastName: "JSM" };
+  const loggedIn = {
+    firstName: "Adrian",
+    lastName: "JSM",
+    email: "jsm@horizon.com",
+  };
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn} />
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalance from "@/components/TotalBalance";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Adrian" };
+  const loggedIn = {
+    firstName: "Adrian",
+    lastName: "JSM",
+    email: "contact@jsmastery.pro",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -14,14 +19,19 @@ const Home = () => {
             user={loggedIn.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
+          <TotalBalance
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
         </header>
-        <TotalBalance
-          accounts={[
-          ]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
-        />
+        RECENT TRANSACTION
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transaction={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   );
 };
